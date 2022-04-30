c7f9feeec3733e2d63d11bfdf9886ed4c00acb28
Ian Goodacre <Ian.Goodacre@xtra.co.nz>
2022-05-01 07:32:59 +1200

    Change test to plain javascript

---
+++ test/test.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+const mocha = require("mocha");
+const pEvent = require("p-event");
+const EPub = require('../../epub');
+mocha.describe('EPub', () => {
+    mocha.it('init', () => {
+        const epub = new EPub('./example/alice.epub');
+        assert.strictEqual(epub.imageroot, `/images/`);
+    });
+    mocha.it('basic parsing', async () => {
+        const epub = new EPub('./example/alice.epub');
+        epub.parse();
+        await pEvent(epub, 'end');
+        assert.ok(epub.metadata.title);
+        assert.equal(epub.metadata.title, "Alice's Adventures in Wonderland");
+        assert.equal(epub.toc.length, 14);
+        assert.ok(epub.toc[3].level);
+        assert.ok(epub.toc[3].order);
+        assert.ok(epub.toc[3].title);
+        assert.ok(epub.toc[3].href);
+        assert.ok(epub.toc[3].id);
+        assert.strictEqual(epub.imageroot, `/images/`);
+    });
+    mocha.it('supports empty chapters', () => {
+        var branch = [{ navLabel: { text: '' } }];
+        const epub = new EPub();
+        var res = epub.walkNavMap(branch, [], []);
+        assert.ok(res);
+    });
+    mocha.it('raises descriptive errors', async () => {
+        const epub = new EPub('./example/alice_broken.epub');
+        try {
+            epub.parse();
+            await pEvent(epub, 'end');
+        }
+        catch (err) {
+            assert.ok(err.message.includes('Parsing container XML failed in TOC: Invalid character in entity name'));
+            return;
+        }
+        assert.fail('should not get here');
+    });
+});
